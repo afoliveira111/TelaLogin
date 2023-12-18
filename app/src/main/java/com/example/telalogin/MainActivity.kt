ca715db5490f8d626b6e782eb02dbbf2d8662960
Felipe <felipetermometria@hotmail.com>
2023-12-18 18:02:13 -0300

    Create Username and password, Compose.

--- app/src/main/java/com/example/telalogin/MainActivity.kt
+++ app/src/main/java/com/example/telalogin/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.telalogin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.telalogin.ui.theme.TelaLoginTheme
 
@@ -22,9 +31,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Col
+                    AuthScreen()
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AuthScreen() {
+    Column {
+        var username by remember {
+            mutableStateOf("")
+        }
+        var password by remember {
+            mutableStateOf("")
+        }
+        TextField(
+            value = username,
+            onValueChange = { newValue ->
+                username = newValue
+
+            })
+        TextField(
+            value = password,
+            onValueChange = {
+                password = it
+            }, visualTransformation = PasswordVisualTransformation())
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Entrar")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AuthScreenPreview() {
+    TelaLoginTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AuthScreen()
         }
     }
 }
