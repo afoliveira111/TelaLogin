20f33c4d6d41dddfc96e9a6c72d79f15e5105468
Felipe <felipetermometria@hotmail.com>
2023-12-28 17:16:56 -0300

    Dependencies Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,9 @@ android {
 
 dependencies {
 
+    val nav_version = "2.6.0"
+
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
